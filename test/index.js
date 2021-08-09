401da87eb560cb502f0c46f7c6311f429570da7f
alexander-morris <al@openkoi.com>
2021-08-09 01:51:03 -0300

    ff

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 const swicw = require("..");
 const Arweave = require("arweave");
 const smartweave = require("smartweave");
+const fs = require('fs');
 
 // 9BX6HQV5qkGiXV6hTglAuPdccKoEP_XI2NNbjHv5MMM main contract
 // 0Z4z_Z6tLza640a9aB6Y4wsUHO80i2JAgOa5rZZ7TsA new task contract
@@ -39,6 +40,22 @@ async function main() {
   console.log(`Done in ${t4 - t3}`);
 
   console.log("\nswicw matches SmartWeave?", JSON.stringify(res1) === JSON.stringify(res2));
+
+  console.log("\nswicw1 matches swicw2?", JSON.stringify(res3) === JSON.stringify(res2));
+
+  await writeOutput(1, res1);
+  await writeOutput(2, res2);
+  await writeOutput(3, res3);
 }
 
 main().then(() => console.log("Terminated"));
+
+async function writeOutput (num, data) {
+  var outputFile = __dirname + "/" + num + ".json";
+  var json = JSON.stringify(data);
+  fs.writeFile(outputFile, json, {}, function (err, res) {
+    if (err) console.error("output write " + num + " failed with error ", err);
+    console.log("write succeeded for " + num, res);
+  });
+
+}
